61c6986edc0976817bf3de0dffdd0e6fba9f8643
frluc <frluc.perso+github@gmail.com>
2018-03-06 11:30:06 +0100

    add Computing GC Content solution

---
+++ GC/GC.py
@@ -0,0 +1,33 @@
+#Rosalind #GC
+
+def parse(fasta, sequences):
+    input_file = open(fasta, "r")
+    for line in input_file:
+        if line.startswith(">"):
+            name = line[1:].strip()
+            sequences[name] = ""
+        else:
+            sequences[name] += line.strip()
+
+def seq_gc(seq):
+    gc = (seq.count("G") + seq.count("C"))/float(len(seq))
+    return gc
+
+sequences = {}
+
+parse("fasta.txt",sequences)
+
+gc_content = {}
+
+for key in sequences:
+    gc_content[key] = seq_gc(sequences[key])
+
+max_gc_value = max(gc_content.values())
+max_gc_key = ""
+
+for key in gc_content:
+    if gc_content[key] == max_gc_value:
+        max_gc_key = key
+
+print(max_gc_key)
+print(round(max_gc_value, 6)*100)
